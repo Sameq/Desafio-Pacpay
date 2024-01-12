c06296219d8d3c532df8fa65b09bfab942208559
Sameq <sameque14.campos@hotmail.com>
2024-01-12 14:50:46 -0300

    service user

---
+++ src/main/java/com/picpay/picpay/service/UserService.java
@@ -0,0 +1,34 @@
+package com.picpay.picpay.service;
+
+import com.picpay.picpay.domain.User;
+import com.picpay.picpay.domain.UserType;
+import com.picpay.picpay.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.concurrent.ExecutionException;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void validateTransection(User sander, BigDecimal amount) throws Exception {
+        if(sander.getUserType() == UserType.MERCHANT){
+            throw new Exception("Usuario do tipo Logista não é autorizado!");
+        }
+        if(sander.getBalance().compareTo(amount) < 0){
+            throw new Exception("Saldo insuficiente");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception{
+        return this.userRepository.findById(id).orElseThrow(() -> new Exception("Usuário não encontrado!"));
+    }
+
+    public void saveUser(User user){
+        this.userRepository.save(user);
+    }
+}
